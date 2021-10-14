ae5c98703a31ecff7475fd118bc0ea892039b9c2
Oliver Wolf <owolf@tecracer.de>
2021-10-14 11:54:42 +0200

    TASK002

--- Terraform/LABS/main.tf
+++ Terraform/LABS/main.tf
@@ -7,7 +7,29 @@ resource "aws_instance" "webserver" {
   ami           = "ami-0cd855c8009cb26ef"
   instance_type = "t2.nano"
 
+  vpc_security_group_ids = [aws_security_group.web.id]
+
   tags = {
     Name = "Oliver's Webserver"
+  }
+}
+
+resource "aws_security_group" "web" {
+  name_prefix = "web-access"
+  description = "Allow access to the server from the web"
+  ingress {
+    from_port = 80
+    to_port = 80
+    protocol = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+  egress {
+    from_port = 0
+    to_port = 0
+    protocol = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+  tags = {
+    Name = "web-access"
   }
 }
