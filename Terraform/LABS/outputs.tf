a1d57382ecf78943c665bcb14d72ecccfc47aa7f
Oliver Wolf <owolf@tecracer.de>
2021-10-14 15:20:02 +0200

    TASK012

--- Terraform/LABS/outputs.tf
+++ Terraform/LABS/outputs.tf
@@ -2,3 +2,13 @@ output "public_ip" {
   description = "Public IP Address"
   value = aws_instance.webserver.*.public_ip
 }
+
+output "public_subnets" {
+  description = "List of IDs of public subnets"
+  value = module.vpc.public_subnets
+}
+
+output "vpc_id" {
+  description = "VPC ID"
+  value = module.vpc.vpc_id
+}
