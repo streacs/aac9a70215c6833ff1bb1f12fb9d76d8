77e1b43ddf60f2db541735615c8be42d0bc85f17
Oliver Wolf <owolf@tecracer.de>
2021-10-15 10:49:09 +0200

    added some examples

--- Terraform/000/main.tf
+++ Terraform/000/main.tf
@@ -1,17 +1,55 @@
+/*
+Condition example
+*/
+variable "enable" {
+  type    = bool
+  default = true
+}
+
 resource "aws_s3_bucket" "app" {
-  bucket  = "terraform-oliver-wolf"
-  acl     = "public-read"
+  count = var.enable == true ? 1 : 0
+
+  bucket = "terraform-oliver-wolf"
+  acl    = "public-read"
   website { index_document = "index.html" }
 }
 
 resource "aws_s3_bucket_object" "app-index" {
-  bucket       = aws_s3_bucket.app.id
+  count = var.enable == true ? 1 : 0
+
+  bucket       = aws_s3_bucket.app.0.id
   key          = "index.html"
   content      = "<h1>Terraform simplest sample!</h1>"
   content_type = "text/html"
   acl          = "public-read"
 }
 
-output "static-url" {
-  value = aws_s3_bucket.app.website_endpoint
+/*
+Dynamic terraform Workspace example
+*/
+locals {
+  counts = {
+    default = 1
+    production = 5
+  }
+  count = lookup(local.counts, terraform.workspace, "")
+}
+
+resource "aws_s3_bucket" "this" {
+  count = local.count
+
+  bucket = "terraform-ws-oliver-wolf-${[count.index]}"
+  acl    = "public-read"
+  website { index_document = "index.html" }
+}
+
+/*
+Combined Example
+*/
+resource "aws_s3_bucket" "this2" {
+  count = var.enable == true ? local.count : 0
+
+  bucket = "terraform-ws-oliver-wolf-${[count.index]}"
+  acl    = "public-read"
+  website { index_document = "index.html" }
 }
